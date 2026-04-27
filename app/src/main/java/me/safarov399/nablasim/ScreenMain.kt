2b4d7dcc9b4acf4e2421a37d38fc7bb6618efa70
safarov399 <74855408+safarov399@users.noreply.github.com>
2026-04-27 09:02:53 +0400

    Improved ideal gas screen design, added additional buttons, added string resources.

--- app/src/main/java/me/safarov399/nablasim/ScreenMain.kt
+++ app/src/main/java/me/safarov399/nablasim/ScreenMain.kt
@@ -1,12 +1,13 @@
 package me.safarov399.nablasim
 
+import android.content.Context
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 @Composable
-fun ScreenMain(onNavigateToSecond: () -> Unit) {
+fun ScreenMain(onNavigateToSecond: () -> Unit, context: Context) {
     Button(onClick = onNavigateToSecond) { 
-        Text("Hello")
+        Text(context.getString(R.string.EOS))
     }
 }
